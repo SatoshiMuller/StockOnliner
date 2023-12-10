cc06d5b0d0e5d64680a1cba89e255c4d2c16bd06
Satoshi Muller <ilya.vasiliev0@yandex.ru>
2023-12-10 12:46:26 +0300

    Добавьте файлы проекта.

---
+++ StockOnliner/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockOnliner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StockExchangeMonitor stockExchangeMonitor = new StockExchangeMonitor();
+            stockExchangeMonitor.PriceChangeHandler = ShowPrice;
+            stockExchangeMonitor.Start();
+
+
+        }
+        public static void ShowPrice(int price)
+        {
+            Console.WriteLine($"Новая цена сейчас: {price}");
+            Console.ReadKey();
+        }
+
+    }
+}
+
